d65f5e5dcbcf42bf4a545c7c4bfa3df9a6203f24
Luca Lusso <lussoluca@Lucas-MacBook-Pro.local>
2016-04-15 09:04:49 +0200

    Removed owlcarousel from home page

--- web/themes/ddd2016/js/home-page.js
+++ web/themes/ddd2016/js/home-page.js
@@ -9,22 +9,22 @@
   Section1();
   Section3();
 
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-      0:{
-        items:1
-      },
-      600:{
-        items:3
-      },
-      1000:{
-        items:5
-      }
-    }
-  });
+  // $('.owl-carousel').owlCarousel({
+  //   loop:true,
+  //   margin:10,
+  //   nav:true,
+  //   responsive:{
+  //     0:{
+  //       items:1
+  //     },
+  //     600:{
+  //       items:3
+  //     },
+  //     1000:{
+  //       items:5
+  //     }
+  //   }
+  // });
 
 
 // INTRO ********************************************************************************************************************
